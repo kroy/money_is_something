466ac295d6db683e0a69859a2148bb409e7e7862
kroy <kroy.xc@gmail.com>
2014-02-21 11:57:18 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoneyIsSomething::Application.config.secret_token = '63905a30fef8c46368506daa905167491f1be423054553407be74195876fb287173f15f210cea8c866566a8b79eddb9d7b87cd86b6151c8979aacb3e1c7a7088'
